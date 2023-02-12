8b3d038db1f96aaadd7d33d594b35abecd0d2558
JesperZamora <jesp78m6@stud.kea.dk>
2023-02-12 02:20:36 +0100

    opacity logo

--- src/test/java/com/example/portfolioassignment/PortfolioAssignmentApplicationTests.java
+++ src/test/java/com/example/portfolioassignment/PortfolioAssignmentApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PortfolioAssignmentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
