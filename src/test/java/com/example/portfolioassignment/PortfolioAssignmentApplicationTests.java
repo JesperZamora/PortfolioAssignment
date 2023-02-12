fb9017d682ac1c3ca04c1fd9c6c2ce3ae6b6c48d
JesperZamora <jesp78m6@stud.kea.dk>
2023-02-12 02:07:51 +0100

    Initial commit

---
+++ src/test/java/com/example/portfolioassignment/PortfolioAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolioassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
